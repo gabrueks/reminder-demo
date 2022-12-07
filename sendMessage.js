8959df689095399755663221007c5694e48d2f8a
gabrueks <gabriel03899@hotmail.com>
2022-12-07 07:07:23 -0300

    fix token and sid

--- sendMessage.js
+++ sendMessage.js
@@ -3,7 +3,7 @@ const Twilio = require("twilio");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 console.log(process.env)
-const twilioClient = Twilio(accountSid, authToken);
+const twilioClient = Twilio("AC9a502b3b0913e0ab4f2c0c9c1eb0349f", "8f568ef561435b0fba1e518fa4cee4ef");
 
 module.exports = {
     sendMessage: async function (message, to) {
