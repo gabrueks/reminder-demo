6c7f69cb928413db92dc2772ee58dea24fcb8775
gabrueks <gabriel03899@hotmail.com>
2022-12-07 07:00:40 -0300

    try catch

--- sendMessage.js
+++ sendMessage.js
@@ -7,12 +7,16 @@ const twilioClient = Twilio(accountSid, authToken);
 
 module.exports = {
     sendMessage: async function (message, to) {
-        await twilioClient
-        .messages
-        .create({
-            body: message,
-            from: 'whatsapp:+18444314315',
-            to: `whatsapp:+55${to}`
-        });
+        try {
+            await twilioClient
+            .messages
+            .create({
+                body: message,
+                from: 'whatsapp:+18444314315',
+                to: `whatsapp:+55${to}`
+            });
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
