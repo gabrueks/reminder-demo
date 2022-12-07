9bae4bc4a355aa6823d4a3f1d7d5fc6a2ff2774e
gabrueks <gabriel03899@hotmail.com>
2022-12-07 07:02:11 -0300

    add log

--- sendMessage.js
+++ sendMessage.js
@@ -2,7 +2,7 @@ const Twilio = require("twilio");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-
+console.log(process.env)
 const twilioClient = Twilio(accountSid, authToken);
 
 module.exports = {
